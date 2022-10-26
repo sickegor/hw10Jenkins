2670f21b9208bebdc1cf0a2904ae0831a033be6e
elevoshkin <elevoshkin@rvision.ru>
2022-10-26 17:11:00 +0300

    first commit

---
+++ src/test/java/tests/demoqa/RegistrationFormTests.java
@@ -0,0 +1,51 @@
+package tests.demoqa;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class RegistrationFormTests extends TestBase {
+    @DisplayName("Fill Form")
+    @Test
+    void fillFormTest() {
+        step("Open registrations form", () -> {
+            open("/automation-practice-form");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+            executeJavaScript("$('footer').remove()");
+            executeJavaScript("$('#fixedban').remove()");
+        });
+        step("Fill form", () -> {
+            $("#firstName").setValue("qweqwr");
+            $("#lastName").setValue("fesgfesgs");
+            $("#userEmail").setValue("afaafw@awfafea.com");
+            $("#genterWrapper").$(byText("Other")).click(); // best
+            $("#userNumber").setValue("1234567890");
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption("July");
+            $(".react-datepicker__year-select").selectOption("2008");
+            $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+            $("#subjectsInput").setValue("Math").pressEnter();
+            $("#hobbiesWrapper").$(byText("Sports")).click(); // best
+            $("#uploadPicture").uploadFromClasspath("img/1.png");
+            $("#currentAddress").setValue("Some address 1");
+            $("#state").click();
+            $("#stateCity-wrapper").$(byText("NCR")).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText("Delhi")).click();
+            $("#submit").click();
+        });
+        step("Check form results", () -> {
+            $(".modal-dialog").should(appear);
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+
+            $(".table-responsive table").shouldHave(text("qweqwr"), text("fesgfesgs"),
+                    text("afaafw@awfafea.com"), text("1234567890"));
+        });
+
+    }
+}
---
+++ src/test/java/tests/simple/DisabledTests.java
@@ -0,0 +1,21 @@
+package tests.simple;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DisabledTests {
+    @Test
+    @Disabled
+    void test1() {
+        assertTrue(false);
+    }
+    
+    @Test
+    @Disabled("Any text here")
+    void test2() {
+        assertTrue(false);
+    }
+
+}
